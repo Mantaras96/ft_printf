f01627f545b13d94106c9112414de781154517ec
Albert <albert.mantaras@gmail.com>
2022-02-05 16:17:38 +0100

    Quitar struct

--- src/ft_printf.c
+++ src/ft_printf.c
@@ -5,42 +5,8 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
-// Struct con todo lo que necesitamos para nuestro printf
-typedef struct s_print                       
-{                        
-      va_list  args;    // arg to print out                    
-      int   wdt;        // width                
-      int   prc;        // precision              
-      int   zero;       // zero padding               
-      int   pnt;        // .     
-      int   dash;       // -               
-      int   tl;         // total_length (return value)              
-      int   sign;       // pos or neg number               
-      int   is_zero;    // is number zero                 
-      int   perc;       // %               
-      int   sp;         // space flag ' '            
-}    t_print;
-
-
-//Inicializamos la struct con 0
-t_print *start_print(t_print *print)
-{
-    print->wdt = 0;
-    print->prc = 0;
-    print->zero = 0;
-    print->pnt = 0;
-    print->dash = 0;    
-    print->tl = 0;
-    print->sign = 0;
-    print->is_zero = 0;
-    print->perc = 0;
-    print->sp = 0;
-    return (print);
-}
-
 ft_putstr(char *str){
     size_t i;
-
     i = 0;
     while (str[i] != '\0'){
         write(1, &str[i], 1 );
@@ -49,9 +15,20 @@ ft_putstr(char *str){
 
 }
 
-void ft_print_char(t_print *tab)
+void ft_putchar(int var)
 {
+    write(1, &var, 1);
+}
 
+void ft_printstr(char * s){
+    size_t i;
+
+    i = 0;
+    while (s[i])
+    {
+        ft_putchar(s[i]);
+		i++;
+	}153
 }
 
 int ft_evaluate_param(va_list args, char *str, int i)
@@ -59,6 +36,7 @@ int ft_evaluate_param(va_list args, char *str, int i)
     if (str[i] == 'c') // int
         ft_printchar(va_arg(args, int));
     if (str[i] == 's') // char *
+        ft_printstr(va_arg(args, char *)
         //Funcion
     if (str[i] == 'p') // unsigned long long
         //Funcion
@@ -80,14 +58,9 @@ int ft_evaluate_param(va_list args, char *str, int i)
 
 int ft_printf(const char *input, ...)
 {
-    t_print *str;
     size_t i;
     va_list  args;
 
-    str = malloc(sizeof(t_print));
-    if (!str)
-        return (0);
-    start_print(str);
     va_start(args, input);
     i = 0;
     while (input[i] != '\0')
@@ -101,9 +74,6 @@ int ft_printf(const char *input, ...)
         }
         i++;
     }
-    printf("%s", str->args);
-
-
     return (0);
 }
 
